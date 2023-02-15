acf4edbeff43aa8ed6994b09980792dec1e556ed
Eloise Ryser <eloiseskye@gmail.com>
2023-02-15 15:33:00 -0700

    Adding part 3 of analysis

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -20,3 +20,15 @@ total_summary <- suspension_coil_table %>% summarize(Mean=mean(PSI), Median=medi
 # Create lot summary table
 lot_summary <- suspension_coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
 
+# Part 3
+# All lots
+t.test(suspension_coil_table$PSI,mu=1500)
+
+# Lot one
+t.test(subset(suspension_coil_table,Manufacturing_Lot=="Lot1")$PSI,mu=1500)
+
+# Lot two
+t.test(subset(suspension_coil_table,Manufacturing_Lot=="Lot2")$PSI,mu=1500)
+
+# Lot three
+t.test(subset(suspension_coil_table,Manufacturing_Lot=="Lot3")$PSI,mu=1500)

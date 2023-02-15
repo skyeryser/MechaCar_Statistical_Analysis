9e742280f5b7c56f8baf9821590b85105d312a5f
Eloise Ryser <eloiseskye@gmail.com>
2023-02-15 13:08:23 -0700

    Adding part 2 analysis tables

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -1,6 +1,7 @@
 library(dplyr)
 require(ggplot2)
 
+# Part 1
 mpg_table <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mpg_table) #generate multiple linear regression model
@@ -8,4 +9,14 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mpg_table)) #generate summary statistics
 
 plt <- ggplot(mpg_table,aes(x=vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,y=mpg)) #import dataset into ggplot2
+
+# Part 2
+# Create suspension coil table
+suspension_coil_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+# Create total summary table
+total_summary <- suspension_coil_table %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+
+# Create lot summary table
+lot_summary <- suspension_coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
 
